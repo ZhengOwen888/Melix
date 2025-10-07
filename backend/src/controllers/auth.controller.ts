770b29bcc8b2c7eabb6b935f078f525b66c698b4
Owen Zheng <owenzheng922@gmail.com>
2025-10-07 14:08:11 -0400

    added auth route to backend, endpoints are still incomplete

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+
+export const signup = async (req: Request, res: Response): Promise<any> => {};
+export const login = async (req: Request, res: Response): Promise<any> => {};
+export const logout = async (req: Request, res: Response): Promise<any> => {};
