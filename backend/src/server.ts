18d789c258de200637535a2f225a9998e738aa1a
Owen Zheng <owenzheng922@gmail.com>
2025-10-08 09:59:00 -0400

    updated gitignore file, updated script commands, added dev and prod environemnts, move environment configuration to top of file in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,36 @@
-import express from "express";
+// Choose the enviroment base on NODE_ENV
 import dotenv from "dotenv";
+const envPath = `.env.${process.env.NODE_ENV || "development"}`;
+dotenv.config({ path: envPath });
+
+import express, { type Application } from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./db/connectdb.js";
 
-const startServer = async () => {
+const startServer = async (): Promise<void> => {
   try {
     await connectDB(); // connect to database
 
-    dotenv.config();
-
-    const app = express();
-    const PORT = process.env.PORT || 5000;
+    const app: Application = express();
+    const PORT: number = parseInt(process.env.PORT || "5000", 10);
 
     app.use(express.json()); // use to parse json
     app.use(cookieParser());
 
     app.use("/api/auth", authRoutes);
 
-    app.listen(PORT, () => {
-      console.log(`✅ App listening on port: ${PORT}`);
+    app.listen(PORT, (): void => {
+      console.log(
+        `✅ App listening on port: ${PORT}, (Node Env: ${process.env.NODE_ENV})`
+      );
     });
   } catch (error: unknown) {
-    console.error("❌ Error starting server: ", error);
+    if (error instanceof Error) {
+      console.error("❌ Error starting server: ", error.message);
+    } else {
+      console.error("❌ Error starting server: ", error);
+    }
   }
 };
 
