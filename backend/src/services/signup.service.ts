68b10371f4e4cfb7da2a326875f2c4f196ea2f08
Owen Zheng <owenzheng922@gmail.com>
2025-10-10 18:15:30 -0400

    updated signup service: checking if user already exist in the database

---
+++ backend/src/services/signup.service.ts
@@ -0,0 +1,26 @@
+import { User } from "../models/user.model.js";
+
+interface SignupInput {
+  email: string;
+  username: string;
+  password: string;
+}
+
+export const signupService = async ({
+  email,
+  username,
+  password,
+}: SignupInput): Promise<void> => {
+  if (!email || !username || !password) {
+    throw new Error("SignUp - Missing required fields");
+  }
+  const userExistByEmail = await User.findOne({ email });
+  if (userExistByEmail) {
+    throw new Error("SignUp - Email already Taken");
+  }
+
+  const userExistByUsername = await User.findOne({ username });
+  if (userExistByUsername) {
+    throw new Error("SignUp - Username already Taken");
+  }
+};
